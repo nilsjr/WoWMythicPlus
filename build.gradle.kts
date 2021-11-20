73f79d4e8c6cf7a89869375c63420cbbf30d589b
Nils Druyen <n.druyen@nanogiants.de>
2021-11-20 12:31:05 +0100

    chore: update ftp plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
   kotlin("plugin.serialization") version "1.5.31" apply false
   id("org.jetbrains.compose") version "1.0.0-beta5" apply false
   id("com.github.ben-manes.versions") version "0.39.0"
-  id("de.nilsdruyen.gradle-ftp-upload-plugin") version "0.0.1" apply false
+  id("de.nilsdruyen.gradle-ftp-upload-plugin") version "0.0.2" apply false
 }
 
 group = "de.nilsdruyen"
