03867ee98044a83c9f6310a2d62901b66fc5215a
Nils Druyen <nils.druyen@fressnapf.com>
2022-12-21 22:02:51 +0100

    Fix role enum serialization

--- character-data/src/commonMain/kotlin/de/nilsdruyen/mythicplus/character/enums/Role.kt
+++ character-data/src/commonMain/kotlin/de/nilsdruyen/mythicplus/character/enums/Role.kt
@@ -1,9 +1,13 @@
 package de.nilsdruyen.mythicplus.character.enums
 
 import de.nilsdruyen.mythicplus.character.utils.Constants
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 enum class Role {
   TANK,
+  @SerialName("HEALING")
   HEALER,
   DPS
 }
